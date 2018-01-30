1fe53a48d4b6d1a4a3ec1ca5d0cee1a05c2c8b53
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2018-01-30 17:05:10 -0200

    a

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,11 +1,21 @@
 defmodule Cards do
 
   def create_deck do
-    ["1", "2", "3"]
+    as = ["1", "2", "3"]
+    bs = ["A", "B", "C"]
+
+    for b <- bs, a <- as do
+        "#{a} - #{b}"
+    end
+
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 
 end
